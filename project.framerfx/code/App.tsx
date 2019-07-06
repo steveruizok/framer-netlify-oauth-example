3a3f4fc36b3c896532ef90286136874a5f0c3597
Steve <steveruizok@gmail.com>
2019-07-06 10:52:01 +0100

    Updates design, I think.

--- project.framerfx/code/App.tsx
+++ project.framerfx/code/App.tsx
@@ -1,27 +1,163 @@
 import * as React from "react"
-import { Override } from "framer"
-
+import { Override, Data } from "framer"
+import * as nanogist from "nanogist"
+import { GistClient } from "./GistClient"
 import { default as Authenticator } from "netlify-auth-providers"
 
-const authenticator = new Authenticator({})
+// @ts-ignore
+import { data as MOCK_DATA } from "./MockData.json"
 
-export const Button: Override = () => {
-    const authenticate = () => {
-        authenticator.authenticate(
+const EXAMPLE_TOKEN = "eaa8fbe4e81f5f2193afbeb43a3943ff17ac2d68"
+
+let gistClient
+let authToken
+
+// state
+
+const appState = Data({
+    gist: null,
+    gists: MOCK_DATA,
+    currentPage: 0,
+    pageTitle: "Posts",
+    backAction: null as any,
+})
+
+// Events
+
+const authenticate = async () => {
+    if (navigator.userAgent.includes("FramerX")) {
+        authToken = EXAMPLE_TOKEN
+        gistClient = GistClient(authToken)
+        const gists = MOCK_DATA
+        loadGists(gists)
+    } else {
+        const authenticator = new Authenticator({}).authenticate(
             { provider: "github", scope: "user" },
-            function(err, data) {
-                if (err) {
-                    console.log("error")
-                } else {
-                    console.log(
-                        "Authenticated with GitHub. Access Token: " + data.token
-                    )
-                }
+            async (err, data) => {
+                authToken = data.token
+                gistClient = GistClient(authToken)
+                const gists = await gistClient.all()
+                loadGists(gists)
             }
         )
     }
+}
 
+const loadGists = async data => {
+    appState.gists = data
+    appState.currentPage = 1
+
+    if (data[0]) {
+        appState.pageTitle = "Gists for " + data[0].owner.login
+    }
+}
+
+const loadGist = async gist => {
+    const data = await gistClient.get(gist.id)
+
+    appState.gist = data
+    appState.currentPage = 2
+    appState.pageTitle = "Gist for " + data.owner.login
+    appState.backAction = () => {
+        appState.pageTitle = "Gists for " + data.owner.login
+        appState.currentPage = 1
+    }
+}
+
+// authenticate
+
+export const Button: Override = () => {
     return {
         onTap: authenticate,
+    }
+}
+
+// Overrides
+
+export function UsingExampleFlag(): Override {
+    return {
+        y: navigator.userAgent.includes("FramerX") ? 0 : 1000,
+    }
+}
+
+// Top navigation for current tab / back
+export function Header(): Override {
+    return {
+        title: appState.pageTitle,
+        leftLink: appState.backAction ? "Back" : "",
+        leftIcon: appState.backAction ? "chevron-left" : "none",
+        onLeftTap: () => appState.backAction && appState.backAction(),
+    }
+}
+
+export function NavigationPage(): Override {
+    return {
+        currentPage: appState.currentPage,
+    }
+}
+
+export const Gists: Override = () => {
+    return {
+        items: appState.gists.map(gist => {
+            return {
+                text: gist.files[Object.keys(gist.files)[0]].filename,
+                message:
+                    gist.description && gist.description.length > 24
+                        ? gist.description.slice(0, 24) + "..."
+                        : gist.description,
+                component: "icon",
+                onTap: () => loadGist(gist),
+            }
+        }),
+    }
+}
+
+export const GistTitle: Override = () => {
+    const { gist } = appState
+
+    if (!gist) {
+        return {}
+    }
+
+    return {
+        text: gist.description,
+    }
+}
+
+export const GistDate: Override = () => {
+    const { gist } = appState
+
+    if (!gist) {
+        return {}
+    }
+
+    const createdDate = new Date(gist.created_at).toLocaleDateString(
+        "en-gb",
+        {}
+    )
+    const updatedDate = new Date(gist.updated_at).toLocaleDateString(
+        "en-gb",
+        {}
+    )
+
+    return {
+        text: "Created: " + createdDate + " | Updated: " + updatedDate,
+    }
+}
+
+export const GistFiles: Override = () => {
+    const { gist } = appState
+    if (!gist) {
+        return {}
+    }
+
+    const files = Object.keys(gist.files).map(key => gist.files[key])
+
+    return {
+        cards: files.map(file => ({
+            title: file.filename,
+            body: "` " + file.content + " `",
+            height: 480,
+        })),
     }
 }
---
+++ project.framerfx/code/GistClient.tsx
@@ -0,0 +1,54 @@
+export const GistClient = token => {
+    const headers = {
+        "User-Agent": "nanogist",
+    }
+
+    if (token != undefined) headers["Authorization"] = `token ${token}`
+
+    return {
+        all: () => {
+            return fetch("https://api.github.com/gists", {
+                headers,
+            }).then(res => {
+                return res.json()
+            })
+        },
+        get: id => {
+            return fetch("https://api.github.com/gists/" + id, {
+                headers,
+            }).then(res => {
+                return res.json()
+            })
+        },
+        delete: id => {
+            return fetch("https://api.github.com/gists/" + id, {
+                headers,
+                method: "delete",
+            }).then(res => {
+                return res.status == 204 ? true : false
+            })
+        },
+        update: (id, body) => {
+            return fetch("https://api.github.com/gists/" + id, {
+                headers: Object.assign(headers, {
+                    "Content-Type": "application/json",
+                }),
+                method: "patch",
+                body: JSON.stringify(body),
+            }).then(res => {
+                return res.json()
+            })
+        },
+        create: body => {
+            return fetch("https://api.github.com/gists", {
+                headers: Object.assign(headers, {
+                    "Content-Type": "application/json",
+                }),
+                method: "post",
+                body: JSON.stringify(body),
+            }).then(res => {
+                return res.json()
+            })
+        },
+    }
+}
